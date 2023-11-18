e74fba352dd729dbaa894f39330658a8b8b932b2
Evopass <ti.evopass@gmail.com>
2023-11-18 11:41:17 -0300

    mudando porta do servidor

--- main.py
+++ main.py
@@ -19,4 +19,5 @@ def encerrar_servidor():
 
 # Rodar a API
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
