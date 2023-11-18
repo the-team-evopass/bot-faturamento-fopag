7ef40c8b82d2a9d419d49bb15ce0f0a863c2711f
Evopass <ti.evopass@gmail.com>
2023-11-18 13:24:28 -0300

    ajuste-01

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask
 import criarcobranca
-import os
-import signal
 
 app = Flask(__name__)
 
@@ -13,14 +11,8 @@ def home():
 @app.get('/run')
 def executar_bot():
     criarcobranca
-    print('Encerrando a aplicação...')
-    encerrar_servidor()
-    print('Aplicação encerrada')
     return "Executando Bot de Faturamento EVOPASS"
 
-def encerrar_servidor():
-    # Encerre o processo Flask usando o sinal SIGINT
-    os.kill(os.getpid(), signal.SIGINT)
 
 # Rodar a API
 if __name__ == '__main__':
