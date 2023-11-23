9953b84e82d16b506454f8a653625ef682794082
GabrielLimaEvopass <gabriel.lima@academiaevoque.com.br>
2023-11-23 17:57:04 -0300

    Listagens 23/11

--- listagem.py
+++ listagem.py
@@ -4,6 +4,7 @@ import requests
 urlListagemCompany = 'https://us-central1-api-evopass-d943e.cloudfunctions.net/prod/company?expand=companyContacts%2CcompanyAddress%2CcompanyAgreements'
 urlListagemStudent = 'https://us-central1-api-evopass-d943e.cloudfunctions.net/prod/student?expand=dependents%2CstudentContact%2CstudentAgreement%2CstudentAddress'
 urlListagemDependent = 'https://us-central1-api-evopass-d943e.cloudfunctions.net/prod/dependent/'
+urlListagemAgreementStudent = 'https://us-central1-api-evopass-d943e.cloudfunctions.net/prod/student_agreement'
 
 #FUNÇÃO DE LISTAGEM DE EMPRESA
 def ListagemEmpresas(urlListagemCompany):
@@ -39,7 +40,9 @@ def ListagemEmpresas(urlListagemCompany):
         print(f"Erro: {e}")
         return []
 
-#FUNÇÃO DE LISTAGEM DE FUNCIONARIOS
+# ... (código anterior)
+
+# FUNÇÃO DE LISTAGEM DE FUNCIONÁRIOS
 def ListagemFuncionarios(urlListagemStudent, urlListagemCompany):
     try:
         respostaListagemFuncionarios = requests.get(urlListagemStudent)
@@ -47,75 +50,102 @@ def ListagemFuncionarios(urlListagemStudent, urlListagemCompany):
         if respostaListagemFuncionarios.status_code == 200:
             funcionarios = respostaListagemFuncionarios.json()
             empresas = ListagemEmpresas(urlListagemCompany)
-            
-            #Dict para armazenar a contagem de funcionários por empresa
+
+            # Dict para armazenar a contagem de funcionários por empresa
             contagem_por_empresa = {}
-            
+
             for funcionario in funcionarios:
                 statusReason = funcionario['statusReason']
 
                 if statusReason == "ATIVO":
                     funcionario_id = funcionario['id']
-                    
-                    #Filtro empresas correspondentes ao funcionário
-                    empresas_correspondentes = [empresa for empresa in empresas if empresa['id'] == funcionario_id]
-                    
+
+                    # Filtro empresas correspondentes ao funcionário
+                    empresas_correspondentes = [empresa for empresa in empresas if empresa['id'] == funcionario['companyId']]
+
                     if empresas_correspondentes:
                         for empresa_correspondente in empresas_correspondentes:
                             nome_empresa = empresa_correspondente['tradeName']
-                            
-                            #Atualiza a contagem para a empresa correspondente
+
+                            # Atualiza a contagem para a empresa correspondente
                             contagem_por_empresa[nome_empresa] = contagem_por_empresa.get(nome_empresa, 0) + 1
-            
-            print("")
-            print("")
+
             print("\nQuantidade de funcionários por empresa em implantação:")
             for empresa, contagem in contagem_por_empresa.items():
                 print(f"{empresa}: {contagem}")
-            
+
         else:
             print(f"Erro na requisição. Código de Status: {respostaListagemFuncionarios.status_code}")
 
     except Exception as e:
         print(f"Erro: {e}")
 
-#FUNÇÃO DE LISTAGEM DE DEPENDENTES
+# FUNÇÃO DE LISTAGEM DE DEPENDENTES
 def ListagemDependentes(urlListagemCompany, urlListagemStudent, urlListagemDependent):
     try:
         respostaListagemDependentes = requests.get(urlListagemDependent)
-        print("")
-        print("")
         if respostaListagemDependentes.status_code == 200:
             dependentes = respostaListagemDependentes.json()
             empresas = ListagemEmpresas(urlListagemCompany)
-            
+
             contagem_por_empresa = {}
-            
+
             for dependente in dependentes:
                 statusReason = dependente['statusReason']
 
                 if statusReason == "ATIVO":
                     dependente_id = dependente['id']
-                    
-                    #Filtro empresas correspondentes ao dependente
-                    empresas_correspondentes = [empresa for empresa in empresas if empresa['id'] == dependente_id]
-                    
+
+                    # Filtro empresas correspondentes ao dependente
+                    empresas_correspondentes = [empresa for empresa in empresas if empresa['id'] == dependente['companyId']]
+
                     if empresas_correspondentes:
                         for empresa_correspondente in empresas_correspondentes:
                             nome_empresa = empresa_correspondente['tradeName']
-                            
-                            #Atualiza a contagem para a empresa correspondente
+
+                            # Atualiza a contagem para a empresa correspondente
                             contagem_por_empresa[nome_empresa] = contagem_por_empresa.get(nome_empresa, 0) + 1
-            
+
             print("\nQuantidade de dependentes por empresa em implantação:")
             for empresa, contagem in contagem_por_empresa.items():
                 print(f"{empresa}: {contagem}")
-            
+
         else:
             print(f"Erro na requisição. Código de Status: {respostaListagemDependentes.status_code}")
 
     except Exception as e:
-        print(f"Erro: {e}") 
+        print(f"Erro: {e}")
+
+# FUNÇÃO DE LISTAGEM DE CONTRATOS DE ESTUDANTES
+def ListagemAgreementStudent(urlListagemAgreementStudent, urlListagemStudent):
+    try:
+        respostaListagemAgreementStudent = requests.get(urlListagemAgreementStudent)
+        if respostaListagemAgreementStudent.status_code == 200:
+            agreementStudents = respostaListagemAgreementStudent.json()
+            students = ListagemFuncionarios(urlListagemStudent)
+
+            for agreementStudent in agreementStudents:
+                typeAgreementStudent = agreementStudent['type']
+
+                if typeAgreementStudent == "F":
+                    agreementStudent_id = agreementStudent['id']
+
+                    # Filtro funcionário correspondente ao contrato
+                    students_correspondentes = [student for student in students if agreementStudent_id['id'] == agreementStudent['studentId']]
+
+                    if students_correspondentes:
+                        print("Contrato de Estudante do Tipo F encontrado para os seguintes estudantes:")
+                        for student_correspondente in students_correspondentes:
+                            print(f"Estudante ID: {student_correspondente['id']}, Nome: {student_correspondente['nome']}")
+
+        else:
+            print(f"Erro na requisição. Código de Status: {respostaListagemAgreementStudent.status_code}")
+
+    except Exception as e:
+        print(f"Erro: {e}")
 
 if __name__ == "__main__":
+    ListagemEmpresas(urlListagemCompany)
+    ListagemFuncionarios(urlListagemStudent, urlListagemCompany)
     ListagemDependentes(urlListagemCompany, urlListagemStudent, urlListagemDependent)
+    ListagemAgreementStudent(urlListagemAgreementStudent, urlListagemStudent)
