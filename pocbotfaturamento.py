f3726eb5ffe547aba211b47463c5e47ffe0842d5
GabrielLimaEvopass <gabriel.lima@academiaevoque.com.br>
2023-11-23 14:24:41 -0300

    Poc bot de faturamento 23/11

--- pocbotfaturamento.py
+++ pocbotfaturamento.py
@@ -1,59 +1,121 @@
 import requests
 
-urlListagemEmpresas = 'https://us-central1-api-evopass-d943e.cloudfunctions.net/prod/company?expand=companyContacts%2CcompanyAddress%2CcompanyAgreements'
-urlListaFuncionarios = 'https://us-central1-api-evopass-d943e.cloudfunctions.net/prod/student?expand=dependents%2CstudentContact%2CstudentAgreement%2CstudentAddress'
+#URLS DAS APIS
+urlListagemCompany = 'https://us-central1-api-evopass-d943e.cloudfunctions.net/prod/company?expand=companyContacts%2CcompanyAddress%2CcompanyAgreements'
+urlListagemStudent = 'https://us-central1-api-evopass-d943e.cloudfunctions.net/prod/student?expand=dependents%2CstudentContact%2CstudentAgreement%2CstudentAddress'
+urlListagemDependent = 'https://us-central1-api-evopass-d943e.cloudfunctions.net/prod/dependent/'
 
-
-def ListaEmpresas(urlListagemEmpresas):
+#FUNÇÃO DE LISTAGEM DE EMPRESA
+def ListagemEmpresas(urlListagemCompany):
     try:
-        respostaListagemEmpresas = requests.get(urlListagemEmpresas)
-
+        respostaListagemEmpresas = requests.get(urlListagemCompany)
         if respostaListagemEmpresas.status_code == 200:
-            empresas = respostaListagemEmpresas.json()
-            listaEmpresas = empresas['data']
-            print("Lista de Empresas:")
-
+            empresasJson = respostaListagemEmpresas.json()
+            listaEmpresas = empresasJson['data']
+            print("Lista de Empresas em Implantação:")
+            contagem_empresas_implantacao = 0
+            
+            #Loop para coletar dados das empresas
             for empresa in listaEmpresas:
+                empresa_cnpj = empresa['cnpj']
                 empresa_tradeName = empresa['tradeName']
-                empresa_id = empresa['id']
-                print(empresa_id, empresa_tradeName)
+                empresa_companyStatus = empresa['companyStatus']
+                
+                #Logica para filtrar apenas as empresas que estão em implantacao
+                if empresa_companyStatus == "EM IMPLANTACAO":
+                    print(empresa_cnpj, empresa_tradeName)
+                    contagem_empresas_implantacao += 1
+            
+            print("")
+            print(f"Total de empresas em implantação: {contagem_empresas_implantacao}")
             return listaEmpresas
-
+        
         else:
             print(f"Erro na requisição. Código de Status: {respostaListagemEmpresas.status_code}")
             return []
+        
 
     except Exception as e:
         print(f"Erro: {e}")
         return []
 
-def ListaFuncionarios(urlListaFuncionarios):
+#FUNÇÃO DE LISTAGEM DE FUNCIONARIOS
+def ListagemFuncionarios(urlListagemStudent, urlListagemCompany):
     try:
-        respostaListagemFuncionarios = requests.get(urlListaFuncionarios)
+        respostaListagemFuncionarios = requests.get(urlListagemStudent)
 
         if respostaListagemFuncionarios.status_code == 200:
             funcionarios = respostaListagemFuncionarios.json()
-            empresas = ListaEmpresas(urlListagemEmpresas)
-            print("")
-            print("Lista de funcionários por empresas:")
-            for funcionariosFiltrados in funcionarios:
-                funcionariosFiltrados_statusReason = funcionariosFiltrados['statusReason']
+            empresas = ListagemEmpresas(urlListagemCompany)
+            
+            #Dict para armazenar a contagem de funcionários por empresa
+            contagem_por_empresa = {}
+            
+            for funcionario in funcionarios:
+                statusReason = funcionario['statusReason']
 
-                if funcionariosFiltrados_statusReason == "ATIVO":
-                    funcionariosFiltrados_Id = funcionariosFiltrados['id']
-
-                    empresasCorrespondentes = [empresa for empresa in empresas if empresa['id'] == funcionariosFiltrados_Id]
+                if statusReason == "ATIVO":
+                    funcionario_id = funcionario['id']
                     
-                    if empresasCorrespondentes:
-                        for empresaCorrespondente in empresasCorrespondentes:
-                            nomeEmpresaCorrespondente = empresaCorrespondente['tradeName']
-                            funcionariosFiltrados_firstName = funcionariosFiltrados['firstName']
-                            print(f"Funcionários da empresa {nomeEmpresaCorrespondente}: {funcionariosFiltrados_firstName}")
-                    else:
-                        print(f"O funcionário {funcionariosFiltrados['firstName']} não está cadastrado em uma empresa.")
+                    #Filtro empresas correspondentes ao funcionário
+                    empresas_correspondentes = [empresa for empresa in empresas if empresa['id'] == funcionario_id]
+                    
+                    if empresas_correspondentes:
+                        for empresa_correspondente in empresas_correspondentes:
+                            nome_empresa = empresa_correspondente['tradeName']
+                            
+                            #Atualiza a contagem para a empresa correspondente
+                            contagem_por_empresa[nome_empresa] = contagem_por_empresa.get(nome_empresa, 0) + 1
+            
+            print("")
+            print("")
+            print("\nQuantidade de funcionários por empresa em implantação:")
+            for empresa, contagem in contagem_por_empresa.items():
+                print(f"{empresa}: {contagem}")
+            
+        else:
+            print(f"Erro na requisição. Código de Status: {respostaListagemFuncionarios.status_code}")
 
     except Exception as e:
         print(f"Erro: {e}")
 
+#FUNÇÃO DE LISTAGEM DE DEPENDENTES
+def ListagemDependentes(urlListagemCompany, urlListagemStudent, urlListagemDependent):
+    try:
+        respostaListagemDependentes = requests.get(urlListagemDependent)
+        print("")
+        print("")
+        if respostaListagemDependentes.status_code == 200:
+            dependentes = respostaListagemDependentes.json()
+            empresas = ListagemEmpresas(urlListagemCompany)
+            
+            contagem_por_empresa = {}
+            
+            for dependente in dependentes:
+                statusReason = dependente['statusReason']
+
+                if statusReason == "ATIVO":
+                    dependente_id = dependente['id']
+                    
+                    #Filtro empresas correspondentes ao dependente
+                    empresas_correspondentes = [empresa for empresa in empresas if empresa['id'] == dependente_id]
+                    
+                    if empresas_correspondentes:
+                        for empresa_correspondente in empresas_correspondentes:
+                            nome_empresa = empresa_correspondente['tradeName']
+                            
+                            #Atualiza a contagem para a empresa correspondente
+                            contagem_por_empresa[nome_empresa] = contagem_por_empresa.get(nome_empresa, 0) + 1
+            
+            print("\nQuantidade de dependentes por empresa em implantação:")
+            for empresa, contagem in contagem_por_empresa.items():
+                print(f"{empresa}: {contagem}")
+            
+        else:
+            print(f"Erro na requisição. Código de Status: {respostaListagemDependentes.status_code}")
+
+    except Exception as e:
+        print(f"Erro: {e}") 
+
 if __name__ == "__main__":
-    ListaFuncionarios(urlListaFuncionarios)
+    ListagemDependentes(urlListagemCompany, urlListagemStudent, urlListagemDependent)
