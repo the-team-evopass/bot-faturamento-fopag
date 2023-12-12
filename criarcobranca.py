fcbc882c1b5198dda07a9a68fc1ea4ac9c283b9d
GabrielLimaEvopass <gabriel.lima@academiaevoque.com.br>
2023-12-12 18:18:16 -0300

    12/12

--- criarcobranca.py
+++ criarcobranca.py
@@ -1,47 +1,56 @@
-# CRIANDO COBRANÇA PARA TODOS OS CLIENTES DO ASAAS
 import requests
 from coletadedados import relacao_ativos
 from datavencimento import calcular_data_vencimento
 
-#Api do asaas
-api_key = '$aact_YTU5YTE0M2M2N2I4MTliNzk0YTI5N2U5MzdjNWZmNDQ6OjAwMDAwMDAwMDAwMDAwNTg2MTM6OiRhYWNoXzU5ODI0NzYxLTBiNGQtNDQwNC1iZTAxLWM1OTAyOTQ2NGRlZg=='
+def criar_cobrancas_para_clientes(api_key):
+    # URLs do Asaas
+    url_listar_clientes = 'https://sandbox.asaas.com/api/v3/customers'
+    url_criar_cobranca = 'https://sandbox.asaas.com/api/v3/payments'
 
-#URLs do asaas
-urlListarClientes = 'https://sandbox.asaas.com/api/v3/customers'
-urlCriarCobranca = 'https://sandbox.asaas.com/api/v3/payments'
+    # Cabeçalho
+    headers = {
+        'Content-Type': 'application/json',
+        'access_token': api_key
+    }
 
-#Cabeçalho
-headers = {
-    'Content-Type': 'application/json',
-    'access_token': api_key
-}
+    # Obter lista de clientes
+    response = requests.get(url_listar_clientes, headers=headers)
+    
+    if response.status_code != 200:
+        print(f"Erro ao listar os clientes. Status Code: {response.status_code}")
+        print(f"Resposta: {response.text}")
+        return
 
-#Listar clientes
-response = requests.get(urlListarClientes, headers=headers)
+    customers = response.json()['data']
 
-if response.status_code == 200:
-    print("Lista de clientes obtida com sucesso:")
-    response_data = response.json()
-
-    #Identificar os clientes de cada 'data' e criar uma cobrança para cada cliente
-    for customer in response_data['data']:
+    # Iterar sobre os clientes e criar cobranças
+    valor = 0  # Substituir pelo valor no banco de dados
+    for customer in customers:
         customer_id = customer['id']
         customer_name = customer['name']
+
+        # Dados da cobrança
         invoice_data = {
-            'customer': customer_id,  # ID do cliente
-            'billingType': 'BOLETO',  #BOLETO = FOPAG
-            'value': relacao_ativos,  # Valor da cobrança            (Substituir pelo valor no banco de dados)
-            'dueDate': calcular_data_vencimento(),  # Data de vencimento  (Substituir pelo valor no banco de dados)
+            'customer': customer_id,
+            'billingType': 'BOLETO',
+            'value': valor,
+            'dueDate': calcular_data_vencimento(),
             'description': 'Cobrança para cliente',
         }
 
-        #Criar a cobrança para os clientes
-        invoice_response = requests.post(urlCriarCobranca, headers=headers, json=invoice_data)
+        # Criar a cobrança
+        invoice_response = requests.post(url_criar_cobranca, headers=headers, json=invoice_data)
 
         if invoice_response.status_code == 200:
             print(f"Cobrança criada com sucesso para o cliente ID: {customer_name}")
         else:
             print(f"Erro ao criar a cobrança para o cliente ID: {customer_name}")
-else:
-    print(f"Erro ao listar os clientes. Status Code: {response.status_code}")
-    print(f"Resposta: {response.text}")
+            print(f"Status Code: {invoice_response.status_code}")
+            print(f"Resposta: {invoice_response.text}")
+
+if __name__ == "__main__":
+    # Api do Asaas
+    api_key = '$aact_YTU5YTE0M2M2N2I4MTliNzk0YTI5N2U5MzdjNWZmNDQ6OjAwMDAwMDAwMDAwMDAwNTg2MTM6OiRhYWNoXzU5ODI0NzYxLTBiNGQtNDQwNC1iZTAxLWM1OTAyOTQ2NGRlZg=='
+    
+    # Chamar a função principal
+    criar_cobrancas_para_clientes(api_key)
--- teste.py
+++ teste.py
@@ -3,7 +3,7 @@ import requests
 # URLs DAS APIs
 urlAllCompany = 'https://us-central1-api-evoppass-dev.cloudfunctions.net/v1/company?expand=companyContacts%2CcompanyAddress%2CcompanyAgreements'
 urlAllStudent = 'https://us-central1-api-evoppass-dev.cloudfunctions.net/v1/student?expand=dependents%2CstudentContact%2CstudentAgreement%2CstudentAddress%2Ccompany'
-urlAllDependent = 'https://us-central1-api-evoppass-dev.cloudfunctions.net/v1/dependent?expand=dependentAgreement'
+urlAllDependent = 'https://us-central1-api-evoppass-dev.cloudfunctions.net/v1/dependent?expand=student%2CdependentContact%2CdependentAgreement%2CdependentAddress'
 
 # Listagem de Empresas e a data corte
 respostaAllCompany = requests.get(urlAllCompany)
@@ -21,7 +21,7 @@ if respostaAllCompany.status_code == 200:
     listaDependentes = dependentJson
 
     # Contador de empresas
-    contagem_empresas_implantacao = 0
+    contagem_empresas = 0
     # Contador de titulares e dependentes
     contagem_titulares_empresa = 0
     contagem_dependentes_empresa = 0
@@ -33,43 +33,59 @@ if respostaAllCompany.status_code == 200:
         empresa_companyStatus = empresa['companyStatus']  # Status da empresa
         empresa_cutoffDate = empresa['cutoffDate']  # Data corte
         empresa_companyAgreements_value = empresa['companyAgreements']  # Valor que a empresa paga
-        for valor in empresa_companyAgreements_value:
-            empresa_value = valor['value']
-        print(f"Empresa: {empresa_tradeName}, paga {empresa_value}")
-        contagem_value_titular = 0
-        contagem_value_dependente = 0
+        if empresa_companyStatus == "EM IMPLANTACAO":
+            for valor in empresa_companyAgreements_value:
+                empresa_value = valor['value']
+            contagem_value_titular = 0
+            contagem_value_dependente = 0
+            contagem_empresas += 1
 
-        # Loop para coletar dados dos Titulares
-        for titular in listaTitulares:
-            titular_firstName = titular['firstName']
-            titular_status = titular['status']
-            titular_startValidity = titular['startValidity']
-            titular_cpf = titular['cpf']
-            titular_agreement = titular['studentAgreement']
-            titular_dependents = titular['dependents']
-            titular_company = titular['company']
-            for companhia_titular in titular_company:
-                nome_empresa_titular = titular_company['tradeName']
-            for titular_agreement_value in titular_agreement:
-                titular_value = titular_agreement_value['value']
-                contagem_value_titular += titular_value
-            print(f"O titular {titular_firstName} pertence a empresa {nome_empresa_titular} e paga {titular_value} ")
-                
+            # Loop para coletar dados dos Titulares
+            for titular in listaTitulares:
+                titular_firstName = titular['firstName']
+                titular_status = titular['status']
+                titular_startValidity = titular['startValidity']
+                titular_cpf = titular['cpf']
+                titular_agreement = titular['studentAgreement']
+                titular_company = titular['company']
+                if titular_status == True:
+                    for companhia_titular in titular_company:
+                        nome_empresa_titular = titular_company['tradeName']
+                    for titular_agreement_value in titular_agreement:
+                        titular_value = titular_agreement_value['value']
+                    contagem_value_titular += float(titular_value)
+                    
+                    # Loop para coletar dados dos Dependentes dos titulares
+                    for dependente in listaDependentes:
+                        dependente_firstName = dependente['firstName']
+                        dependente_status = dependente['status']
+                        dependente_startValidity = dependente['startValidity']
+                        dependente_cpf = dependente['cpf']
+                        dependente_agreement = dependente['dependentAgreement']
+                        dependente_student = dependente['student']['cpf']
+                        if dependente_status == True:
+                            contagem_dependentes_empresa += 1
+                            for dependente_agreement_value in dependente_agreement:
+                                dependente_value = dependente_agreement_value['value']
+                                if dependente_student == titular_cpf:
+                                    contagem_value_dependente += 1
+                                    contagem_value_dependente += float(dependente_value)
+                        else:
+                            print(f"O dependente {dependente_firstName}, não está ATIVO")
 
-            # Loop para coletar dados dos Dependentes dos titulares
-            for dependente in titular_dependents:
-                dependente_firstName = dependente['firstName']
-                dependente_status = dependente['status']
-                dependente_startValidity = dependente['startValidity']
-                dependente_cpf = dependente['cpf']
-                dependente_agreement = dependente['dependentAgreement']
-                contagem_dependentes_empresa += 1
-                for dependente_agreement_value in dependente_agreement:
-                    dependente_value = dependente_agreement_value['value']
-                    contagem_value_dependente += dependente_value
-                print(f"O dependente: {dependente_firstName} pertence ao titular {titular_firstName} e paga {dependente_value}")
-                contagem_value_dependente += 1  
-    
+                    print(f"A empresa {empresa_tradeName} pagará {empresa_value} por valor de vida, {contagem_value_titular} por titular e {contagem_value_dependente} por dependente.")
+                    valor_total_empresa = float(empresa_value) + float(contagem_value_titular) + float(contagem_value_dependente)
+                    print(valor_total_empresa)
+
+                    
+
+                else:
+                    print(f"O titular {titular_firstName}, não está ATIVO")
+
+            print(contagem_empresas)
+
+        else:
+            print(f"A empresa {empresa_tradeName}, não está em IMPLANTACAO")
 
 else:
     print(f"Erro na requisição. Código de Status: {respostaAllCompany.status_code}")
